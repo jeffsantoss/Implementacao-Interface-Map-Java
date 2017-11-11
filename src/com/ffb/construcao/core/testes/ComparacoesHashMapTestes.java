b64de1bf79569ad742be2db9c2869d153454da0a
jeffsantos <jeffsantoss>
2017-11-11 18:37:53 -0300

    Primeiro commit e inclusão de classes & testes

---
+++ src/com/ffb/construcao/core/testes/ComparacoesHashMapTestes.java
@@ -0,0 +1,5 @@
+package com.ffb.construcao.core.testes;
+
+public class ComparacoesHashMapTestes {
+
+}
---
+++ src/com/ffb/construcao/core/testes/HashMapCaelumTest.java
@@ -0,0 +1,14 @@
+package com.ffb.construcao.core.testes;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+import org.junit.jupiter.api.Test;
+
+public class HashMapCaelumTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
